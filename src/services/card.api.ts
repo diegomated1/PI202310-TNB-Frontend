9f488ffab6214c60575bf888ce7a57c6ce7b9d45
diegomated1 <diegocardwr@gmail.com>
2023-02-17 11:18:38 -0500

    connection to admin-card api

---
+++ src/services/card.api.ts
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import ICard from '../interfaces/ICard';
+
+class CardsApi{
+
+    baseUrl: string
+    constructor(){
+        this.baseUrl = 'http://192.168.0.2:3000';
+    }
+
+    insert(card:ICard){
+        return new Promise(async(res, rej)=>{
+            try{
+                const {data} = await axios.post(`${this.baseUrl}/cards`, {...card});
+                res(data.data);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    getById(_id:string):Promise<ICard>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const {data} = await axios.get(`${this.baseUrl}/cards/${_id}`);
+                res(data.data);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    getAll():Promise<ICard[]>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const {data} = await axios.get(`${this.baseUrl}/cards`);
+                res(data.data);
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+}
+
+
+export default new CardsApi();
+
