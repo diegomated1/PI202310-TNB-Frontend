f2f5194a9c18adc76e32d69b603bae04a4e6de6e
diegomated1 <diegocardwr@gmail.com>
2023-02-25 14:03:11 -0500

    add image upload and inputs for create card page

--- src/pages/admin_cards/AdminCards.tsx
+++ src/pages/admin_cards/AdminCards.tsx
@@ -1,33 +1,75 @@
+import { FormEvent, useEffect, useState } from "react"
 import AdminCardsNavBar from "../../components/AdminCardsNavBar"
 import Button from "../../components/Button"
 import Input from "../../components/Input"
+import cardApi from "../../services/card.api";
 
 export default function AdminCards(){
+
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [image, setImage] = useState<File>();
+
+    const handleCreateCard = async (e:FormEvent)=>{
+        try{
+            e.preventDefault();
+            console.log(name);
+            console.log(description);
+            var data = await cardApi.insert({name, description, type: 0}, image!);
+            //console.log(data);
+        }catch(error){
+            console.log(error);
+        }
+    }
+
+    useEffect(()=>{
+        console.log(image);
+    }, [image]);
+
     return(
         <div className="w-screen h-screen flex flex-wrap flex-col">
             <AdminCardsNavBar/>
             <div className="w-full h-[calc(100%-70px)] bg-gray-300 flex justify-center items-center">
                 <div className="w-full h-full bg-gray-300">
                     <div className="w-full h-20 flex items-center pl-10">
-                        <h1 className="text-4xl md:text-5xl">Edicion cartas Epicas</h1>
+                        <h1 className="text-4xl md:text-5xl">Creacion de cartas</h1>
                     </div>
                     <div className="w-full h-[calc(100%-5rem)] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 p-10 gap-10">
                         <div className="col-span-1 flex justify-center items-center">
-                            <div className="border border-black rounded-md w-[300px] h-[440px]">
-
+                            <div className="border border-black rounded-md w-[300px] h-[440px] flex flex-col items-center">
+                                <img src={(image) ? URL.createObjectURL(image!) : ''}></img>
+                                <div className="mt-auto relative w-[50px] h-[50px] rounded-[25px]">
+                                    <input 
+                                        className="cursor-pointer opacity-0 absolute top-0 left-0 w-full h-full z-20"
+                                        onChange={(e)=>{setImage(e.target.files![0])}} type={'file'}
+                                        accept={'image/png, image/jpg, image/jpeg'}
+                                    />
+                                    <div 
+                                        className="z-10 border border-gray-400 shadow-xl absolute top-0 left-0 w-full h-full rounded-[25px] flex items-center justify-center">
+                                        <span className="text-5xl">+</span>
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-span-1 flex flex-col items-center">
+                        <form onSubmit={handleCreateCard} className="col-span-1 flex flex-col items-center">
                             <label className="w-[90%] h-10 mb-10">
                                 <strong>Nombre:</strong> <br/>
-                                <Input/>
+                                <Input onChange={setName}/>
                             </label>
                             <label className="w-[90%] h-10 mb-14">
                                 <strong>Descripcion:</strong>  <br/>
-                                <Input/>
+                                <Input onChange={setDescription}/>
+                            </label>
+                            <label className="w-[90%] h-10 mb-14">
+                                <strong>Tipo de carta:</strong>  <br/>
+                                <Input onChange={setDescription}/>
+                            </label>
+                            <label className="w-[90%] h-10 mb-14">
+                                <strong>Heroe al que pertenece:</strong>  <br/>
+                                <Input onChange={setDescription}/>
                             </label>
                             <Button text="Crear"/>
-                        </div>
+                        </form>
                         <div className="col-span-1 flex flex-col">
                             <div><span className="text-xl"><strong>Reporte de cambios</strong></span></div>
                             <div><span className="text-gray-500 text-sm" >14/02/2023</span></div>
