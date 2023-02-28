4cbb8d1f5e39620ab120fd2d1b4261c2a46f0e8a
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2023-02-28 11:14:08 -0500

    modulo comentarios

--- src/pages/card_details/cardDetails.tsx
+++ src/pages/card_details/cardDetails.tsx
@@ -4,12 +4,28 @@ import Button from "../../components/Button";
 import Icons from "../../components/Icons";
 import '../../assets/react.svg';
 import '../../assets/tailwind.css';
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import cardsApi from '../../services/card.api';
 
 export default function CardDetails() {
+
+    const params = useParams();
+    const [card, setCard] = useState();
+
+    const handleGetCard = async () => {
+        const data = await cardsApi.getById(params.id_card!);
+        console.log(data);
+    }
+
+    useEffect(() => {
+        handleGetCard();
+    }, []);
+
     return (
         <div className="flex flex-col w-screen h-screen">
             <AdminCardsNavBar />
-            <div className="flex-1 w-full borderborder-gray-500 bg-gray-100 flex flex-col justify-center items-center p-5">
+            <div className="flex-1 w-full borderborder-gray-500 bg-gray-300 flex flex-col justify-center items-center p-5">
                 <div className="w-[80%] h-full grid grid-cols-2 lg:grid-cols-5">
                     <div className="col-span-2 p-5">
                         <div className="w-full h-full border border-gray-500 bg-white rounded-xl ">
@@ -53,7 +69,7 @@ export default function CardDetails() {
                                     <Button text="-" type="buttonlarge" />
                                 </div>
                                 <div className="flex-1 p-1">
-                                    <div className="border border-gray-500 rounded-lg w-full h-full flex justify-center items-center">
+                                    <div className="border border-separate rounded-lg w-full h-full flex justify-center items-center">
                                         <span>0</span>
                                     </div>
                                 </div>
@@ -74,6 +90,7 @@ export default function CardDetails() {
                             <div className="p-3 ">
                                 <h3 className="text-xl">Ranking</h3>
                                 <div className="p-3 flex">
+
                                     <Icons icon="star"></Icons>
                                     <Icons icon="star"></Icons>
                                     <Icons icon="star"></Icons>
@@ -99,8 +116,38 @@ export default function CardDetails() {
                             </div>
                         </div>
                     </div>
-                    <div className="col-span-3 p-5">
-                        <div className="w-full h-full border border-gray-500 bg-white rounded-lg"></div>
+                    <div className="col-span-3 p-3">
+                        <div className="w-full h-full border border-gray-500 bg-white rounded-lg">
+                            <div className=" border border-spacing-1  border-black">
+                                <div className="p-1 w-full flex">
+                                    <div className="w-[90%] p-1">
+                                        <h2 className="text-xl p-2"><strong>Usuario1</strong></h2>
+                                    </div>
+                                    <div className=" flex text-right w-[10%] p-1">
+                                        <Icons icon="edit"></Icons>
+                                        <Icons icon="trash"></Icons>
+                                    </div>
+                                </div>
+                                <div className="p-1">
+                                    <h2 className="text-xl p-3">2002-14-34</h2>
+                                    <div className="p-3 flex">
+                                        <Icons icon="star"></Icons>
+                                        <Icons icon="star"></Icons>
+                                        <Icons icon="star"></Icons>
+                                        <Icons icon="star"></Icons>
+                                        <Icons icon="star"></Icons>
+                                    </div>
+                                    <div className="p-3">
+                                        <label className="p-3"> Es un panda gigante que de manera improbable es elegido como el Guerrero
+                                            Dragón. Él es hijo adoptivo del Sr. Ping, y es uno de los estudiantes del
+                                            maestro Shifu. La profecía se refiere a Po como el Guerrero Dragón o guerrero
+                                            de blanco y negro.Es un panda gigante que de manera improbable es elegido</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <br className="bg-black" />
+                        </div>
+
                     </div>
                 </div>
             </div>
