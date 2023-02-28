821f018e22cda885f8d2dd1cc891de4a0cf7cd0d
unknown <admin@BGAI202-005.bga.upb>
2023-02-28 11:09:13 -0500

    empezando el item

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-import AdminCards from "./pages/admin_cards/AdminCards"
-import AdminHeroes from "./pages/admin_heroes/AdminHeroes"
+import AdminCards from "./pages/admin_create_cards/AdminCards"
+import AdminHeroes from "./pages/admin_create_heroes/AdminHeroes"
+import AdminViewGrid from "./pages/admin_view_item/AdminViewItems"
 import Login from "./pages/login/login"
 import Register from "./pages/register/register"
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/admin/heroes" element={<AdminHeroes/>}/>
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
+        <Route path="/admin/grilla" element={<AdminViewGrid/>}/>
       </Routes>
     </BrowserRouter>
   )
