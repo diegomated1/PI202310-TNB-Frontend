7f571f2c21aad9339f91df8b203fdcd29521dcb4
diegomated1 <diegocardwr@gmail.com>
2023-03-16 14:35:15 -0500

    page for creating a new game match

--- src/pages/create_game/CreateGame.tsx
+++ src/pages/create_game/CreateGame.tsx
@@ -1,16 +1,36 @@
-import { useState } from "react";
+import { MouseEventHandler, useEffect, useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/Input";
 import AdminCardsNavBar from "../../components/NavBar";
 import AddPlayerSquare from "./components/AddPlayerSquare";
+import useMatches from "../../hooks/useMatches";
 
-
+/**
+ * Component for creating a new game match.
+ */
 export default function CreateGame(){
 
+    const matches = useMatches();
     const [bet, setBet] = useState(0);
     const [numberPlayers, setNumberPlayers] = useState(2);
+    
+    const [creatingMatch, setCreatingMatch] = useState(0);
 
+    /**
+     * Handler function for creating a new match
+     */
+    const handleCreateMatch = ()=>{
+        if(true){
+            setCreatingMatch(1);
+            matches.createMatch(numberPlayers, bet);
+        }
+    }
 
+    useEffect(()=>{
+        if(matches.match){
+            setCreatingMatch(0);
+        }
+    }, [matches.match]);
 
     return(
         <div className="w-screen h-screen flex flex-col">
@@ -18,37 +38,42 @@ export default function CreateGame(){
             <div className="flex-1 flex">
                 <div className="flex-1 p-12 ">
                     <div className="p-5 w-full h-full flex flex-col bg-gray-100">
-                        <div className="w-full h-16">
+                        <div className="flex-1">
                             <h1 className="text-3xl font-bold">Crear Partida</h1>
                         </div>
-                        <div className="w-full h-24 flex flex-col justify-evenly">
+                        <div className="flex-[2] flex flex-col justify-evenly">
                             <label><strong>Apuesta de la partida:</strong></label>
                             <div className="w-28">
-                                <Input/>
+                                <Input onChange={(e)=>{setBet(parseInt(e))}}/>
                             </div>
                         </div>
-                        <div className="w-full h-24 flex flex-col justify-evenly">
+                        <div className="flex-[2] flex flex-col justify-evenly">
                             <label><strong>Numero de jugadores:</strong></label>
                             <div className="w-full flex">
                                 <div className="w-8 h-8 mr-2">
-                                    <Button text="2" type={(numberPlayers==2)?'buttonYellow':''}/>
+                                    <Button onClick={()=>{setNumberPlayers(2)}} text="2" type={(numberPlayers==2)?'buttonYellow':''}/>
                                 </div>
                                 <div className="w-8 h-8 mr-2">
-                                    <Button text="3" type={(numberPlayers==3)?'buttonYellow':''}/>
+                                    <Button onClick={()=>{setNumberPlayers(3)}} text="3" type={(numberPlayers==3)?'buttonYellow':''}/>
                                 </div>
                                 <div className="w-8 h-8">
-                                    <Button text="4" type={(numberPlayers==4)?'buttonYellow':''}/>
+                                    <Button onClick={()=>{setNumberPlayers(4)}} text="4" type={(numberPlayers==4)?'buttonYellow':''}/>
                                 </div>
                             </div>
                         </div>
-                        <div className="w-full h-56 grid grid-cols-4">
+                        <div className="flex-1 grid grid-cols-4">
                             {[...Array(numberPlayers)].map((x,i)=>(
                                 <AddPlayerSquare key={i}/>
                             ))}
                         </div>
-                        <div className="flex-1 flex justify-end items-center pr-5">
+                        <div className="flex-[2] flex justify-end items-center pr-5">
                             <div className="w-36">
-                                <Button text="Crear partida"/>
+                                <Button onClick={handleCreateMatch} text="Crear partida"/>
+                                {(creatingMatch==1) ? (
+                                    <span className="w-full flex justify-center">Creando partida...</span>
+                                ) : (
+                                    ''
+                                )}
                             </div>
                         </div>
                     </div>
