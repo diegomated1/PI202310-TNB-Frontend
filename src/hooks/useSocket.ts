4505e44cbf2033547d5b3c1d26abd29fc286e4c3
diegomated1 <diegocardwr@gmail.com>
2023-03-16 14:24:43 -0500

    docs

--- src/hooks/useSocket.ts
+++ src/hooks/useSocket.ts
@@ -2,16 +2,20 @@ import { useEffect, useState } from "react";
 import * as io from 'socket.io-client';
 
 /**
- * @param url socket server url 'ws://host:port'
- * @returns {io.Socket} instance of the socket
+ * Custom hook for managing a socket connection. 
+ * 
+ * @param {string} url The URL of the socket server to connect to.
+ * @returns {io.Socket|null} The socket instance or null if it has not been initialized yet.
  */
 export default function useSocket(url:string){
     const [socket, setSocket] = useState<io.Socket|null>(null);
 
     useEffect(() => {
+        // Creates a new socket instance and sets it to the state.
         const newSocket = io.connect(url);
         setSocket(newSocket);
     
+        // Disconnects the socket when the component using the hook unmounts.
         return () => {
             newSocket.disconnect();
         };
