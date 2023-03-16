c0f0718b8e7b599c7cc950fb1a857ea60321eb45
diegomated1 <diegocardwr@gmail.com>
2023-03-16 14:04:45 -0500

    custom hook for socket connection

---
+++ src/hooks/useSocket.ts
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+import * as io from 'socket.io-client';
+
+/**
+ * @param url socket server url 'ws://host:port'
+ * @returns instance of the socket
+ */
+export default function useSocket(url:string){
+    const [socket, setSocket] = useState<io.Socket|null>(null);
+
+    useEffect(() => {
+        const newSocket = io.connect(url);
+        setSocket(newSocket);
+    
+        return () => {
+            newSocket.disconnect();
+        };
+    }, [url]);
+  
+    return socket;
+};
