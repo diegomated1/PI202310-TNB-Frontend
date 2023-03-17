31e1467f77d332c034531e6e65a0d6baa7cc0086
diegomated1 <diegocardwr@gmail.com>
2023-03-17 15:48:47 -0500

    function for get user by id

--- src/services/user.api.ts
+++ src/services/user.api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import IUser from '../interfaces/IUser';
 
 
 interface errorResponse{
@@ -9,7 +10,7 @@ class UserApi{
 
     baseUrl: string
     constructor(){
-        this.baseUrl = process.env.REACT_APP_API_USERS_URL!;
+        this.baseUrl = 'http://192.168.0.11:3002';
     }
 
     auth(){
@@ -50,6 +51,17 @@ class UserApi{
                     withCredentials: true,
                 });
                 res(data)
+            }catch(error){
+                rej(error);
+            }
+        });
+    }
+
+    getById(id_user:string):Promise<IUser>{
+        return new Promise(async(res, rej)=>{
+            try{
+                const {data} = await axios.get(`${this.baseUrl}/user/${id_user}`);
+                res(data.data)
             }catch(error){
                 rej(error);
             }
