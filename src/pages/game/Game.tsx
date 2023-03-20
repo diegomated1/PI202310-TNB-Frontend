48c930ac65dae0924fdb4bcf0b1d7cf2f75d1e69
diegomated1 <diegocardwr@gmail.com>
2023-03-20 10:35:32 -0500

    game page

---
+++ src/pages/game/Game.tsx
@@ -0,0 +1,33 @@
+
+
+export default function Game(){
+
+    return(
+        <div className="w-screen h-screen flex">
+            <div className="w-96 h-full bg-red-100 flex">
+                <div className="w-full h-1/3 bg-red-200 flex flex-col">
+                    <div className="flex-1 flex justify-center items-center text-4xl">
+                        <span>Ronda: 1</span>
+                    </div>
+                    <div className="flex-1 flex justify-center items-center text-4xl">
+                        <span>Turno: kevin</span>
+                    </div>
+                </div>
+            </div>
+            <div className="flex-1 bg-blue-100 flex flex-col">
+                <div className="flex-1 bg-blue-200">
+                    <div className="">
+                        
+                    </div>
+                </div>
+                <div className="flex-1 bg-blue-300">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+
+
---
+++ src/pages/game/components/Hero.tsx
@@ -0,0 +1,11 @@
+
+
+
+
+export default function Hero(){
+    return(
+        <div>
+
+        </div>
+    )
+}
