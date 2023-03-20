32d952ae9738b9dfb2410636e9a37d7f64c99703
diegomated1 <diegocardwr@gmail.com>
2023-03-20 16:53:06 -0500

    game cards

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,10 +1,13 @@
+import Card from "./components/Card";
+import Deck from "./components/Deck";
+import Hero from "./components/Hero";
 
 
 export default function Game(){
 
     return(
         <div className="w-screen h-screen flex">
-            <div className="w-96 h-full bg-red-100 flex">
+            <div className="flex-1 h-full bg-red-100 flex">
                 <div className="w-full h-1/3 bg-red-200 flex flex-col">
                     <div className="flex-1 flex justify-center items-center text-4xl">
                         <span>Ronda: 1</span>
@@ -14,14 +17,22 @@ export default function Game(){
                     </div>
                 </div>
             </div>
-            <div className="flex-1 bg-blue-100 flex flex-col">
-                <div className="flex-1 bg-blue-200">
-                    <div className="">
-                        
+            <div className="flex-[3] flex flex-col">
+                <div className="flex-1 flex items-center">
+                    <div className="flex-1 flex justify-evenly">
+                        <Hero/>
+                        <Hero/>
+                        <Hero/>
                     </div>
                 </div>
-                <div className="flex-1 bg-blue-300">
-
+                <div className="flex-1">
+                    <div className="flex-1 flex justify-evenly">
+                        <Deck/>
+                        <Hero/>
+                        <Card/>
+                        <Card/>
+                        <Card/>
+                    </div>
                 </div>
             </div>
         </div>
