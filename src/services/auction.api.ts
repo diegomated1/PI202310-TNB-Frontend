37402eee3c54a9aba8e292193d7e2f901fd2f0b1
diegomated1 <diegocardwr@gmail.com>
2023-03-31 13:48:00 -0500

    auction api

---
+++ src/services/auction.api.ts
@@ -0,0 +1,37 @@
+import axios from 'axios';
+import IAuction from '../interfaces/IAuction';
+
+class AuctionsApi {
+
+    baseUrl: string
+    constructor() {
+        this.baseUrl = import.meta.env.VITE_SERVER_AUCTION;
+    }
+
+    getById(id_card: string): Promise<IAuction> {
+        return new Promise(async (res, rej) => {
+            try {
+                const { data } = await axios.get(`${this.baseUrl}/auctions/${id_card}`);
+                res(data.data);
+            } catch (error) {
+                rej(error);
+            }
+        });
+    }
+
+    getAll(): Promise<IAuction[]> {
+        return new Promise(async (res, rej) => {
+            try {
+                const { data } = await axios.get(`${this.baseUrl}/auctions`);
+                res(data.data);
+            } catch (error) {
+                rej(error);
+            }
+        });
+    }
+
+}
+
+
+export default new AuctionsApi();
+
