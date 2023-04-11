2038a8126127ac318e81f45e1422ccd7c0ddc128
diegomated1 <diegocardwr@gmail.com>
2023-04-11 15:22:00 -0500

    lobby interface

--- src/interfaces/IMatch.ts
+++
@@ -1,9 +0,0 @@
-
-export default interface IMatch{
-    _id: string,
-    id_owner: string,
-    max_number_players: number,
-    ias: number,
-    players: {id_user: string, id_hero: string}[],
-    min_bet: number
-}
