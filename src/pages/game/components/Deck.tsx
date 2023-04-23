8eeeccaead01534e294f3fab1e5fd8df59041ef3
diegomated1 <diegocardwr@gmail.com>
2023-04-23 15:07:54 -0500

    show cards count

--- src/pages/game/components/Deck.tsx
+++ src/pages/game/components/Deck.tsx
@@ -1,10 +1,13 @@
+import IPlayer from "../interfaces/IPlayer"
 
+interface IDeckProps{
+    cards_in_deck:number
+}
 
-
-export default function Deck(){
+export default function Deck({cards_in_deck}:IDeckProps){
     return(
-        <div className="w-40 h-56 border border-black rounded-lg flex flex-col self-end">
-
+        <div className="w-40 h-56 border border-black rounded-lg flex flex-col self-end items-center justify-center text-4xl">
+            {cards_in_deck}
         </div>
     )
 }
