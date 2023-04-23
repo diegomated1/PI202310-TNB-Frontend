932a838f35b87dcf6e5af0f471863d004803bbaf
diegomated1 <diegocardwr@gmail.com>
2023-04-22 19:45:09 -0500

    update hook for get data and attack

--- src/pages/game/hooks/useGame.ts
+++ src/pages/game/hooks/useGame.ts
@@ -1,75 +1,131 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import useAuth from "../../../hooks/useAuth";
 import useSocket from "../../../hooks/useSocket";
-import IHeroe from "../../../interfaces/IHeroe";
 import IGame from "../interfaces/IGame";
 import IHero from "../interfaces/IHero";
-import IUser from "../interfaces/IUser";
-import IUsers from "../interfaces/IUsers";
+import gameApi from "../../../services/game.api";
+import IResult from "../interfaces/IResult";
+import IPlayer from "../interfaces/IPlayer";
+import IUser from "../../../interfaces/IUser";
 
 
-export default function useGame(){
-
+export default function userGame(id_game:string|null, user:IUser|null|undefined){
+    
     const navigate = useNavigate();
-    const socket = useSocket("ws://localhost:3001");
-    const {user} = useAuth();
-    const [token, setToken] = useState<string|undefined>();
+    const socket = useSocket(import.meta.env.VITE_SOCKET_GAME);
 
     const [game, setGame] = useState<IGame>();
-    const [userGame, setUserGame] = useState<IUser>();
-    const [hero, setHero] = useState<IHero>();
 
-    const [users, setUsers] = useState<IUsers>({});
+    const [players, setPlayers] = useState<{[key:string]:IPlayer}>({});
+    const [heroes, setHeroes] = useState<{[key:string]:IHero}>({});
 
+    /**
+     * Get all game info (game and users)
+     */
     useEffect(()=>{
-        const token = localStorage.getItem('mathToken');
-        console.log(token);
-        if(token){
-            setToken(token);
-        }else{
-            //navigate('/game/lobby/create');
+        if(id_game && user){
+            const handleGetGame = async () => {
+                const {game} = await gameApi.getGameById(id_game);
+                const _players:{[key:string]:IPlayer} = {};
+                const _heroes:{[key:string]:IHero} = {};
+                for(const player of game.players_in_game){
+                    const _player = await gameApi.getUserByGameId(id_game, player);
+                    _players[_player.user._id] = _player.user;
+                    _heroes[_player.hero._id] = _player.hero;
+                }
+                setGame(game);
+                setPlayers(_players);
+                setHeroes(_heroes);
+            }
+            handleGetGame();
         }
-    }, []);
+    }, [user]);
 
     useEffect(()=>{
-        if(socket && user && token){
-            function gameUseJoin(_user:IUser, hero:IHero){
-                if(_user && (_user._id != user!.id_user)  && hero){
-                    _user.id_hero = _user.id_hero.split('-')[1];
-                    hero._id = _user.id_hero;
-                    setUsers(_users=>({..._users, [_user._id]: {
-                        hero, user: _user
-                    }}));
+        if(id_game && socket && user){
+            /**
+             * Event when new player join the game 
+             * @param _user user joined
+             * @param _hero hero of the user
+             */
+            function gameUseJoin(_user:IPlayer, _hero:IHero){
+                if(_user && _hero){
+                    setPlayers(players=>({...players, [_user._id]: _user}));
+                    setHeroes(heroes=>({...heroes, [_hero._id]: _hero}));
                 }
             }
 
-            function gameGetUserInfo
-            (verified:boolean, game:IGame, user:IUser, hero:IHero, _users:IUsers){
-                if(verified){
-                    setGame(game);
-                    setUserGame({...user, id_hero: user.id_hero.split('-')[1]});
-                    setHero(hero);
-                    setUsers(_users);
-                }else{
-                    //navigate('/game/lobby/create');
+            function gameUserLeave(id_user:string){
+                console.log(id_user);
+            }
+
+            function gameStart(game:IGame){
+                console.log(game);
+            }
+
+            function onAttack(result:IResult){
+                if(result.attack>result.defense){
+                    setHeroes((heroes)=>({
+                        ...heroes,
+                        [result.hero._id]: result.hero,
+                        [result.hero_target._id]: result.hero_target
+                    }));
                 }
+            }
+
+            function onPass(turn:string, round?:number){
+                setGame(game=>({...game!, turn, current_round: round??game!.current_round}));
             }
 
             socket.on('game:user:join', gameUseJoin);
-            socket.on('game:get:user:info', gameGetUserInfo);
+            socket.on('game:user:leave', gameUserLeave);
+            socket.on('game:user:attack', onAttack);
+            socket.on('game:user:pass', onPass);
+            socket.on('game:start', gameStart);
 
-            socket.emit('game:get:user:info', user.id_user, token);
+            socket.emit('game:user:join', user.id_user, id_game);
             // Clean up function to remove the event listener when the component unmounts.
             return () => {
                 socket.off('game:user:join', gameUseJoin);
-                socket.off('game:get:user:info', gameGetUserInfo);
+                socket.off('game:user:leave', gameUserLeave);
+                socket.off('game:user:attack', onAttack);
+                socket.off('game:start', gameStart);
+                socket.off('game:user:pass', onPass);
             }
         }
     }, [socket, user]);
 
+    function attack(id_hero_target:string){
+        if(socket && user && game){
+            const id_hero = players[user.id_user].id_hero;
+            socket.emit('game:user:attack', game._id, id_hero, id_hero_target);
+        }
+    }
+
+    function grabCard(){
+        if(socket && user && game){
+            socket.emit('game:user:grab', game._id, user.id_user);
+        }
+    }
+
+    function changeCard(id_card:string){
+        if(socket && user && game){
+            socket.emit('game:user:changeCard', game._id, user.id_user, id_card);
+        }
+    }
+
+    function passTurn(){
+        if(socket && user && game){
+            socket.emit('game:user:pass', game._id, user.id_user);
+        }
+    }
+
     return {
-        game, user: userGame, hero, users
+        game,
+        players, heroes,
+        actions: {
+            attack, grabCard, changeCard, passTurn
+        }
     }
 }
 
