0177e537cbfbf1d303e121ce8e238759cfb1d2cc
diegomated1 <diegocardwr@gmail.com>
2023-04-22 19:48:16 -0500

    add new changes from others components

--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -2,29 +2,72 @@ import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import Deck from "./components/Deck";
 import Hero from "./components/Hero";
-import History from "./components/History";
-import Options from "./components/Options";
+import History from "./components/tab/History";
+import Options from "./components/tab/Options";
 import useGame from "./hooks/useGame";
+import { useSearchParams } from "react-router-dom";
+import IGame from "./interfaces/IGame";
+import Actions from "./components/Actions";
+import Alert from "./components/alerts/alert";
+import IHero from "./interfaces/IHero";
+import useAuth from "../../hooks/useAuth";
+import IUser from "../../interfaces/IUser";
+import userApi from "../../services/user.api";
 
 
 export default function Game(){
 
+    const [searchParams] = useSearchParams();
     const [tab, setTab] = useState(0);
-    const game = useGame();
+
+    const {user} = useAuth();
+    const {game, players, heroes, actions} = useGame(searchParams.get('g'), user);
+
+    const [hero, setHero] = useState<IHero>();
+
+    const [userTurn, setUserTurn] = useState<IUser>();
+
+    const [ups, setUps] = useState(0);
+    const [grabs, setGrabs] = useState(0);
+    const [changes, setChanges] = useState(0);
+    const [attacks, setAttacks] = useState(0);
+    const [pass, setPass] = useState(0);
 
     useEffect(()=>{
-        console.log(game.users);
-    }, [game.users]);
+        if(user && heroes[players[user.id_user].id_hero]){
+            setHero(heroes[players[user.id_user].id_hero]);
+        }
+    }, [heroes]);
+
+    useEffect(()=>{
+        if(game){
+            const handleGetUser = async ()=>{
+                const _user = await userApi.getById(game.turn);
+                setUserTurn(_user);
+            }
+            handleGetUser();
+        }
+    }, [game?.turn]);
+
+    const handleAttack = (id_hero_target:string)=>{
+        if(hero && hero.power>0){
+            actions.attack(id_hero_target);
+        }
+    }
+
+    const handlePassTurn = ()=>{
+        actions.passTurn();
+    }
 
     return(
-        <div className="w-screen h-screen flex">
+        <div className="relative w-screen h-screen flex">
             <div className="flex-1 h-full bg-red-100 flex flex-col">
                 <div className="w-full h-1/3 bg-red-200 flex flex-col">
                     <div className="flex-1 flex justify-center items-center text-4xl">
-                        <span>Ronda: 1</span>
+                        <span>Ronda: {(game && game.current_round) ? game.current_round : '0'}</span>
                     </div>
                     <div className="flex-1 flex justify-center items-center text-4xl">
-                        <span>Turno: kevin</span>
+                        <span>Turno: {userTurn?userTurn.username:''}</span>
                     </div>
                 </div>
                 <div className="flex-1 bg-blue-100 flex flex-col">
@@ -53,20 +96,21 @@ export default function Game(){
             <div className="flex-[3] flex flex-col relative">
                 <div className="flex-1 flex items-center">
                     <div className="flex-1 flex justify-evenly">
-                        {(game && game.users) ? (
-                            Object.values(game.users).map((user, i)=>(
-                                <Hero key={i} id_hero={user.hero._id}/>
-                            ))
-                        ) : (
-                            ''
-                        )}
+                        {Object.values(heroes).filter(hero=>hero._id!=players[user?.id_user!].id_hero).map((_hero, i)=>(
+                            <Hero 
+                                key={i} 
+                                hero={_hero}
+                                canSelect={(hero && hero.power>0)?true:false}
+                                onSelect={handleAttack}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div className="flex-1 flex items-center">
                     <div className="flex-1 flex justify-evenly">
                         <Deck/>
-                        {(game && game.user) ? (
-                            <Hero id_hero={game.user.id_hero}/>
+                        {(user && hero) ? (
+                            <Hero hero={hero} canSelect={false}/>
                         ) : (
                             ''
                         )}
@@ -76,6 +120,11 @@ export default function Game(){
                     </div>
                 </div>
             </div>
+            <Actions 
+                attack={()=>{}} pass={handlePassTurn} 
+                power={hero?hero.power:0}
+                turn={(user && game) ? (user.id_user == game.turn) : false}
+            />
         </div>
     )
 }
