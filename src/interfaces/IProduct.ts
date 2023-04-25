636530c205506c5ec77b10feb11a5ce64ac13cb8
LuxyKolink <santiagolikeaboss22@gmail.com>
2023-04-24 23:53:14 -0500

    corregido interfaz

--- src/interfaces/IProduct.ts
+++ src/interfaces/IProduct.ts
@@ -2,7 +2,6 @@ export default interface IProduct{
     id_product?: number,
     type_object: string,
     id_object: string,
-    id_card: string,
     price: number,
     discount: number,
     availability: number,
