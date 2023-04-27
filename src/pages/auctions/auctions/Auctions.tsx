a144ac27658a8cad6b0b8f8a50fededac70a8834
Diego-Arciniegas <62758837+Diego-Arciniegas@users.noreply.github.com>
2023-04-27 04:48:44 -0500

    l

--- src/pages/auctions/auctions/Auctions.tsx
+++ src/pages/auctions/auctions/Auctions.tsx
@@ -9,17 +9,15 @@ export default function Auctions(){
     const [auctions] = useAuctions();
 
     return(
-        <div className="w-screen h-screen bg-red-200 flex flex-col">
+        <div className="w-screen h-screen bg-gray-300 flex flex-col">
             <AdminCardsNavBar/>
             <div className="flex-1 flex">
                 <div className="flex-1">
 
                 </div>
-                <div className="flex-[3] flex flex-col">
-                    <div className="h-20 bg-gray-200">
-
-                    </div>
-                    <div className="flex-1 bg-gray-300">
+                <div className="flex-[3] p-10 flex flex-col">
+                    
+                    <div className=" flex-1 bg-white border border-gray-500 rounded-lg">
                         <ul className="w-full h-full flex flex-col">
                             {auctions.map((auction, i)=>(
                                 <li key={i}>
